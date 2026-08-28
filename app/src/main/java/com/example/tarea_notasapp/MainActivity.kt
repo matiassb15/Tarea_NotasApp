443eb7edabaf744974ebc09f0d16073a2953f4ed
matiassb15 <salas05alonso@gmail.com>
2026-08-28 09:37:37 -0600

    Diseño Principal

--- app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
+++ app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.tarea_notasapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tarea_notasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.FABAgregarNota.setOnClickListener {
+            Toast.makeText(applicationContext,
+                "has presionado en el FAB",
+                Toast.LENGTH_SHORT).show()
         }
     }
 }
