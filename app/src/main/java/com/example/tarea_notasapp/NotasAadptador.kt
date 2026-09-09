e71f9be66c3f2b171f1c28f9fa84da434d2c608a
Matiassb15 <salas05alonso@gmail.com>
2026-09-08 22:00:20 -0600

    Adaptador

---
+++ app/src/main/java/com/example/tarea_notasapp/NotasAadptador.kt
@@ -0,0 +1,37 @@
+package com.example.tarea_notasapp
+
+import android.content.Context
+import android.renderscript.Type
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotasAadptador (private var notas : List<Nota>, context: Context): RecyclerView.Adapter<NotasAadptador.NotaViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotaViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_nota,parent,false)
+        return NotaViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NotaViewHolder, position : Int) {
+        val nota = notas[position]
+        holder.itemTitulo.text = nota.titulo
+        holder.itemdescripcion.text = nota.description
+    }
+
+    override fun getItemCount(): Int {
+        return notas.size
+    }
+
+    class NotaViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val itemTitulo : TextView = itemView.findViewById(R.id.item_titulo)
+        val itemdescripcion : TextView = itemView.findViewById(R.id.item_descripcion)
+
+
+    }
+    fun refrescarLista (nuevaNota : List<Nota>){
+        notas = nuevaNota
+        notifyDataSetChanged()
+    }
+}
