f8be291b2412a0b94c2e5d38ce907b2a3beb52f4
Matiassb15 <salas05alonso@gmail.com>
2026-09-08 22:08:45 -0600

    Mostrar notas de la BD

--- app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
+++ app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
@@ -8,19 +8,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tarea_notasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var db : NotasDataBaseHelper
+    private lateinit var notasAdapator : NotasAadptador
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db = NotasDataBaseHelper(this)
+
+        notasAdapator = NotasAadptador(db.getAllNotas(),this)
+
+        binding.notasRv.layoutManager = LinearLayoutManager(this)
+        binding.notasRv.adapter = notasAdapator
+
         binding.FABAgregarNota.setOnClickListener {
             startActivity(Intent(applicationContext, AgregarNotaActivity::class.java))
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notasAdapator.refrescarLista(db.getAllNotas())
     }
 }
