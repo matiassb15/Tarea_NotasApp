67de9fcc9ad8967ad396ed534256689fad20fce7
Matiassb15 <salas05alonso@gmail.com>
2026-09-08 20:08:34 -0600

    Validar Campos

--- app/src/main/java/com/example/tarea_notasapp/AgregarNotaActivity.kt
+++ app/src/main/java/com/example/tarea_notasapp/AgregarNotaActivity.kt
@@ -26,16 +26,25 @@ class AgregarNotaActivity : AppCompatActivity() {
         binding.ivGuardarNota.setOnClickListener {
             val titulo = binding.etTitulo.text.toString()
             val descripcion = binding.etDescripcion.text.toString()
-            val nota = Nota(0, titulo, descripcion)
-            db.insertNota(nota)
-            startActivity(Intent(applicationContext, MainActivity::class.java))
-            finishAffinity()
-            Toast.makeText(applicationContext, "Se ha agregado la nota", Toast.LENGTH_SHORT).show()
+
+            if (!titulo.isEmpty() && !descripcion.isEmpty()){
+                guardarNota(titulo,descripcion)
+            }else{
+                Toast.makeText(applicationContext, "Llene los campos", Toast.LENGTH_SHORT).show()
+            }
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun guardarNota(titulo: String, descripcion: String){
+        val nota = Nota(0, titulo, descripcion)
+        db.insertNota(nota)
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+        finishAffinity()
+        Toast.makeText(applicationContext, "Se ha agregado la nota", Toast.LENGTH_SHORT).show()
     }
 }
