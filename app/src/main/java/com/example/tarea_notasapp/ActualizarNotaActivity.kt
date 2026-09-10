99f3aaa4ddfef5347700ac448063db3941add650
Matiassb15 <salas05alonso@gmail.com>
2026-09-10 17:35:41 -0600

    Actualizar nota en BD

--- app/src/main/java/com/example/tarea_notasapp/ActualizarNotaActivity.kt
+++ app/src/main/java/com/example/tarea_notasapp/ActualizarNotaActivity.kt
@@ -1,17 +1,51 @@
 package com.example.tarea_notasapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tarea_notasapp.databinding.ActivityActualizarNotaBinding
 
 class ActualizarNotaActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityActualizarNotaBinding
+    private lateinit var db : NotasDataBaseHelper
+    private var idNota : Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_actualizar_nota)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityActualizarNotaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NotasDataBaseHelper(this)
+
+        idNota = intent.getIntExtra("id_nota", -1)
+        if (idNota == -1){
+            finish()
+            return
+        }
+
+        val nota = db.getIdNota(idNota)
+        binding.etTitulo.setText(nota.titulo)
+        binding.etDescripcion.setText(nota.description)
+
+        binding.ivActualizarNota.setOnClickListener {
+            val tituloNuevo = binding.etTitulo.text.toString()
+            val descripcionNuevo = binding.etDescripcion.text.toString()
+
+            val notaActualizada = Nota (idNota, tituloNuevo , descripcionNuevo)
+            db.updateNota(notaActualizada)
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            Toast.makeText(this, "La nota ha se ha actualizado con éxito", Toast.LENGTH_SHORT).show()
+
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
