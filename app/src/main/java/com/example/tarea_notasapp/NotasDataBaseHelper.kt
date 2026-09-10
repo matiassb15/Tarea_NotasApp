8a9960864b2678405c0492b8c7f360d6050fda13
Matiassb15 <salas05alonso@gmail.com>
2026-09-10 17:17:08 -0600

    Consulta para actualizar una nota

--- app/src/main/java/com/example/tarea_notasapp/NotasDataBaseHelper.kt
+++ app/src/main/java/com/example/tarea_notasapp/NotasDataBaseHelper.kt
@@ -78,5 +78,18 @@ class NotasDataBaseHelper(context: Context) : SQLiteOpenHelper(
         return Nota(id, titulo, descripcion)
     }
 
+    fun updateNota (nota: Nota){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put(COLUMN_TITLE, nota.titulo)
+            put(COLUMN_DESCRIPTION,nota.description)
+        }
+
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(nota.id.toString())
+        db.update(TABLE_NAME, values, whereClause,whereArgs)
+        db.close()
+    }
+
 
 }
